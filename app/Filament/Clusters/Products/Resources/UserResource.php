f8d0f7614be81cead38d55ae93cbb95dfc84d0cf
Ankit Singh <ankitsinggggg@gmail.com>
2025-02-07 12:28:29 +0000

    user

---
+++ app/Filament/Clusters/Products/Resources/UserResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Clusters\Products\Resources;
+
+use App\Filament\Clusters\Products;
+use App\Filament\Clusters\Products\Resources\UserResource\Pages;
+use App\Filament\Clusters\Products\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $cluster = Products::class;
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                                            ->required() // cannot empty
+                                            ->maxLength(255), // max char 255
+
+                Forms\Components\TextInput::make('email')
+                                            ->required() // cannot empty
+                                            ->email() // email validation
+                                            ->maxLength(255), // max char 255
+
+                Forms\Components\TextInput::make('password')
+                                            ->required() // cannot empty
+                                            ->password() //  password text input
+                                            ->revealable() // hide show password
+                                            ->maxLength(255), // max char 255
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('name')->searchable(),
+            Tables\Columns\TextColumn::make('email')->searchable(),
+        ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
